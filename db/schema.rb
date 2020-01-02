cb8cdf0ab20579ead5bafda94c4987e5d8f008c5
Lauren Dove <elledoveinc@gmail.com>
2020-01-01 19:32:21 -0800

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
-    
-    create_table "users", force: :cascade do |t|
-        t.string  "username"
-        t.string  "password"
-        t.decimal "balance"
-      end
+ActiveRecord::Schema.define(version: 1) do
 
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
+    t.decimal "balance"
+  end
 
 end
